d06a7aa219a5a045795c0bdbd29b82df583d0366
Szymon Baranowski <szymon.baranowski@netguru.pl>
2016-02-24 16:08:17 +0100

    Add configuration and use builder to request API

--- lib/picguard.rb
+++ lib/picguard.rb
@@ -1,17 +1,36 @@
 require "picguard/version"
-require "google/apis/vision_v1"
+require "json"
+require "picguard_init"
+require "configuration"
+require "services/builders/request"
+require "services/analyzer"
 
-class Picguard
-  def self.process_picture(path)
-    vision = Google::Apis::VisionV1::VisionService.new
-    vision.key = ''
 
-    image = Google::Apis::VisionV1::Image.new(content: open(path).to_a.join)
-    feature = Google::Apis::VisionV1::Feature.new(max_results: "1", type: "LABEL_DETECTION")
-    image_request =  Google::Apis::VisionV1::AnnotateImageRequest.new(image: image, features:[feature])
-    image_batch = Google::Apis::VisionV1::BatchAnnotateImagesRequest.new(requests: [image_request])
+module Picguard
+  class << self
+    attr_accessor :configuration
+  end
 
-    result = vision.annotate_image(image_batch)
-    puts result.to_h
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  def self.analyze(image_path:,
+                   safe_search: true,
+                   face_detection: true,
+                   threshold_adult: Picguard.configuration.threshold_adult,
+                   threshold_violence: Picguard.configuration.threshold_violence,
+                   threshold_face: Picguard.configuration.threshold_face
+                   )
+
+    Services::Analyzer.new(
+      Services::Builders::Request.new(
+        image_path, safe_search, face_detection
+      ).call,
+      threshold_adult,
+      threshold_violence,
+      threshold_face
+    ).call
   end
 end
