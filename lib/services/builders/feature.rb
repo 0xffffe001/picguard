7c410f120452ca8513696b8e6976757880bd4a36
Tomasz Jaśkiewicz <tomasz.jaskiewicz@netguru.pl>
2016-02-26 12:15:12 +0100

    Add spec for Builders::Feature service

--- lib/services/builders/feature.rb
+++ lib/services/builders/feature.rb
@@ -3,7 +3,7 @@ require "google/apis/vision_v1"
 module Services
   module Builders
     class Feature
-      def initialize(type, max_results = "1")
+      def initialize(type, max_results = '1')
         @type = type
         @max_results = max_results
       end
---
+++ spec/services/builders/feature_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Services::Builders::Feature do
+  let(:type) { 'SAFE_SEARCH_DETECTION' }
+  let(:max_results) { '3' }
+
+  context 'max_results is defined' do
+    subject { described_class.new(type, max_results) }
+
+    describe '#call' do
+      it 'creates valid Google::Apis::VisionV1::Feature object' do
+        expect(subject.call.class.name).to eq 'Google::Apis::VisionV1::Feature'
+      end
+    end
+  end
+
+  context 'max_results is not defined' do
+    subject { described_class.new(type) }
+
+    describe '#call' do
+      it 'creates valid Google::Apis::VisionV1::Feature object' do
+        expect(subject.call.class.name).to eq 'Google::Apis::VisionV1::Feature'
+      end
+    end
+  end
+end
