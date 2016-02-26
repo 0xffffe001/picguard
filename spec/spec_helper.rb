d9538a3d33f3671aad6ebbeef114c465c42be788
Tomasz Jaśkiewicz <tomasz.jaskiewicz@netguru.pl>
2016-02-26 17:46:40 +0100

    Exclude google apis classes from test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
-SimpleCov.start
+SimpleCov.start do
+  add_filter '/lib/google/apis'
+end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'picguard'
