fb6f86f4485f8715969c41fec26c30bf898fc9a0
Szymon Baranowski <szymon.baranowski@netguru.pl>
2016-02-27 16:33:44 +0100

    Add picguard spec with configuration and secret as ENV

--- spec/picguard_spec.rb
+++ spec/picguard_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 describe Picguard do
+  before(:all) do
+    Picguard.configure do |config|
+      config.google_api_key = ENV['GCLOUD_KEY']
+      config.threshold_adult = "POSSIBLE"
+      config.threshold_violence = "UNLIKELY"
+      config.threshold_face = 0.8
+    end
+  end
   it 'has a version number' do
     expect(Picguard::VERSION).not_to be nil
+  end
+
+  describe '#analyze' do
+    subject { described_class.analyze(image_path: "spec/support/img/face-example.jpg") }
+    it 'returns a result as hash' do
+      expect(subject).to be_an_instance_of(Hash)
+    end
   end
 end
